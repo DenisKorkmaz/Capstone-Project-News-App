471b7b09104809440772513ec7ad07e98790f9ff
Denis Korkmaz <deniskorkmaz22@gmail.com>
2023-08-01 11:19:49 +0200

    WrapAppInsideASWRConfig

--- components/Article.js/Article.js
+++ components/Article.js/Article.js
@@ -1,31 +1,38 @@
-import React from 'react';
-import useSWR from 'swr';
+import React from "react";
+import useSWR from "swr";
 import { StyledH1, StyledImage, StyledButton, StyledContainer } from "./styles";
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
-
 export default function Article() {
-  const { data, error } = useSWR('https://www.tagesschau.de/api2/news', fetcher);
+  const { data, error } = useSWR("https://www.tagesschau.de/api2/news");
 
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
+  if (error) {
+    return <div>failed to load</div>;
+  }
+  if (!data) {
+    return <div>loading...</div>;
+  }
 
   return (
     <div>
-      {data.news.map((item) => (
-        item.type === "story" && item.teaserImage?.imageVariants?.["16x9-256"]
-          ? (
-            <>
+      {data.news.map((item) =>
+        item.type === "story" &&
+        item.teaserImage?.imageVariants?.["16x9-256"] ? (
+          <>
             <StyledContainer>
               <StyledH1>{item.title}</StyledH1>
-              <StyledImage src={item.teaserImage?.imageVariants?.["16x9-256"]} />
-              <a href={item?.shareURL} target="_blank" rel="noopener noreferrer">
+              <StyledImage
+                src={item.teaserImage?.imageVariants?.["16x9-256"]}
+              />
+              <a
+                href={item?.shareURL}
+                target="_blank"
+                rel="noopener noreferrer">
                 <StyledButton>zum Artikel</StyledButton>
               </a>
-              </StyledContainer>
-            </>
-          ) : null
-      ))}
+            </StyledContainer>
+          </>
+        ) : null
+      )}
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-
+import { SWRConfig } from "swr";
 import Article from "@/components/Article.js/Article";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export default function App() {
   return (
-    <>
- 
-      <Article>
-      </Article>
-  
-    </>
+    <SWRConfig value={{ fetcher }}>
+      <Article />
+    </SWRConfig>
   );
 }
