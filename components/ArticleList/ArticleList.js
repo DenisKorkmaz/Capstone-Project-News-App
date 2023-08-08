e4ab0f244c9a287608a224b7d73b810687e28717
Denis Korkmaz <deniskorkmaz22@gmail.com>
2023-08-08 09:23:21 +0200

    transfer fetch to pages/index.js

--- components/ArticleList/ArticleList.js
+++ components/ArticleList/ArticleList.js
@@ -1,25 +1,12 @@
 import React from "react";
-import useSWR from "swr";
 import ArticleCard from "../ArticleCard/ArticleCard";
 
-export default function ArticleList() {
-  const { data, error } = useSWR("https://www.tagesschau.de/api2/news");
-
-  if (error) {
-    return <div>failed to load</div>;
-  }
-  if (!data) {
-    return <div>loading...</div>;
-  }
-
-  const stories = data.news.filter(
-    (item) =>
-      item.type === "story" && item.teaserImage?.imageVariants?.["16x9-256"]
-  );
+export default function ArticleList({articles}) {
+  
 
   return (
     <ul>
-      {stories.map((article) => (
+      {articles.map((article) => (
         <li key={article.shareURL}>
         <ArticleCard article={article} />
         </li>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import ArticleList from "@/components/ArticleList/ArticleList";
 import Navigation from "@/components/Navigation/Navigation";
-
-const fetcher = (url) => fetch(url).then((res) => res.json());
+import useSWR from "swr";
 
 export default function Home() {
+  const { data, error } = useSWR("https://www.tagesschau.de/api2/news");
+
+  if (error) {
+    return <div>failed to load</div>;
+  }
+  if (!data) {
+    return <div>loading...</div>;
+  }
+
+  const stories = data.news.filter(
+    (item) =>
+      item.type === "story" && item.teaserImage?.imageVariants?.["16x9-256"]
+  );
   return (
     <>
       <Navigation />
-      <ArticleList />
+      <ArticleList articles={stories} />
     </>
   );
 }
